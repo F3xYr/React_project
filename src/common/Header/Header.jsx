05a064eab9352f95f741bdcca750ae5fe0e94fa2
unknown <sdavide.16.06@gmail.com>
2024-03-05 09:46:25 +0100

    first commit

---
+++ src/common/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './common/Header/Header';
+
+function Header() {
+  return (
+    <div className='App'>
+      <Header/>
+    </div>
+  );
+}
+
+export default Header;
