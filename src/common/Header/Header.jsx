8a26acd3e8fdffe5d83faebd4cff9878c7db9eb3
unknown <sdavide.16.06@gmail.com>
2024-03-05 10:38:08 +0100

    first commit

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,11 +1,21 @@
-import './App.css';
-import Header from './common/Header/Header';
+import React from "react";
+import logo from "../../logo.svg"
+import "./Header.css"
 
-function Header() {
+const Header = () => {
   return (
-    <div className='App'>
-      <Header/>
-    </div>
+    <header className="header">
+      <div className="wrapper">
+      <img src={logo} alt="Logo" className="logo"/>
+        <nav>
+          <ul className="menu">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
